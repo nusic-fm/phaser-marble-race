938df195c1eb5bcec3745a1c416dfb571c657824
logesh2496 <logesh.r24@gmail.com>
2024-08-22 18:51:00 +0530

    fix: auto recording

--- src/game/PhaserGame.tsx
+++ src/game/PhaserGame.tsx
@@ -1,6 +1,7 @@
-import { forwardRef, useLayoutEffect, useRef } from "react";
+import { forwardRef, useLayoutEffect, useRef, useState } from "react";
 import StartGame from "./main";
 import { GameVoiceInfo } from "./scenes/Preloader";
+import * as Tone from "tone";
 
 export interface IRefPhaserGame {
     game: Phaser.Game | null;
@@ -27,6 +28,14 @@ export interface IGameDataParams {
 interface IProps extends IGameDataParams {
     currentActiveScene?: (scene_instance: Phaser.Scene) => void;
 }
+const downloadVideo = (videoUrl: string, name: string) => {
+    const link = document.createElement("a");
+    link.href = videoUrl;
+    link.download = `${name}.webm`; // Set the file name for download
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
 
 export const PhaserGame = forwardRef<IRefPhaserGame, IProps>(
     function PhaserGame(
@@ -49,9 +58,56 @@ export const PhaserGame = forwardRef<IRefPhaserGame, IProps>(
         ref
     ) {
         const game = useRef<Phaser.Game | null>(null!);
+        const gameStarted = useRef<boolean>(true);
+
+        const [mediaRecorder, setMediaRecorder] =
+            useState<null | MediaRecorder>(null);
+        const [isRecording, setIsRecording] = useState(false);
+
+        const startRecording = (canvas: HTMLCanvasElement) => {
+            // const canvas = canvasRef.current;
+            const canvasStream = canvas.captureStream(120); // 30 FPS
+            const audioCtx = Tone.getContext().rawContext;
+            const dest = (audioCtx as any).createMediaStreamDestination();
+            Tone.getDestination().connect(dest);
+            const audioStream = dest.stream;
+
+            // navigator.mediaDevices
+            //     .getUserMedia({ audio: true })
+            //     .then((micStream) => {
+            // Combine the canvas video stream and the audio stream
+            const combinedStream = new MediaStream([
+                ...canvasStream.getTracks(),
+                ...audioStream.getTracks(),
+            ]);
+
+            const recorder = new MediaRecorder(combinedStream, {
+                mimeType: "video/webm; codecs=vp9,opus",
+            });
+
+            const chunks: Blob[] = [];
+            recorder.ondataavailable = (e) => chunks.push(e.data);
+            recorder.onstop = () => {
+                const blob = new Blob(chunks, { type: "video/webm" });
+                downloadVideo(URL.createObjectURL(blob), coverDocId);
+            };
+
+            recorder.start();
+            setMediaRecorder(recorder);
+            setIsRecording(true);
+
+            setTimeout(() => {
+                stopRecording(recorder);
+            }, 60000);
+        };
+
+        const stopRecording = (recorder: MediaRecorder) => {
+            recorder.stop();
+            setIsRecording(false);
+        };
 
         useLayoutEffect(() => {
-            if (game.current === null) {
+            if (gameStarted.current === false) {
                 game.current = StartGame("game-container", {
                     voices,
                     coverDocId,
@@ -74,6 +130,11 @@ export const PhaserGame = forwardRef<IRefPhaserGame, IProps>(
                 } else if (ref) {
                     ref.current = { game: game.current, scene: null };
                 }
+                if (game.current) {
+                    startRecording(game.current.canvas);
+                }
+            } else {
+                gameStarted.current = false;
             }
 
             return () => {
